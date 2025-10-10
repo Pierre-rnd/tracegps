251c99bf2bb58f53358a333922e02d17be2f5858
Francoisgrd <grondin.f@lyceedelasalle.fr>
2025-10-10 09:34:01 +0200

    Functoin goood
    gooood

--- modele/DAO.php
+++ modele/DAO.php
@@ -1068,12 +1068,11 @@ $unId = mb_convert_encoding($uneLigne->id, 'UTF-8', 'ISO-8859-1');
 
     public function creerUneTrace($uneTrace)
     {
-        $ok = false;
+        $sql = "INSERT INTO tracegps_traces (id, dateDebut, dateFin, terminee, idUtilisateur) ";
+        $sql.= " VALUES (:id, :dateDebut, :dateFin, :terminee, :idUtilisateur);";
 
-        $sql = "INSERT INTO tracegps_traces (dateDebut, dateFin, terminee, idUtilisateur)
-                VALUES (:dateDebut, :dateFin, :terminee, :idUtilisateur);";
         $req = $this->cnx->prepare($sql);
-
+        $req->bindValue(":id", $uneTrace->getId(), PDO::PARAM_INT);
         $req->bindValue(":dateDebut", $uneTrace->getDateHeureDebut(), PDO::PARAM_STR);
 
         if ($uneTrace->getDateHeureFin() == null)
@@ -1087,6 +1086,32 @@ $unId = mb_convert_encoding($uneLigne->id, 'UTF-8', 'ISO-8859-1');
         $ok = $req->execute();
 
         $req->closeCursor();
+        return $ok;
+    }
+
+    public function supprimerUneTrace($idTrace)
+    {
+        
+
+        try {
+            
+            $sql = "DELETE FROM tracegps_points WHERE idTrace = :idTrace;";
+            $req = $this->cnx->prepare($sql);
+            $req->bindValue(":idTrace", $idTrace, PDO::PARAM_INT);
+            $req->execute();
+            $req->closeCursor();
+
+            
+            $sql = "DELETE FROM tracegps_traces WHERE id = :idTrace;";
+            $req = $this->cnx->prepare($sql);
+            $req->bindValue(":idTrace", $idTrace, PDO::PARAM_INT);
+            $ok = $req->execute();
+            $req->closeCursor();
+
+        } catch (Exception $e) {
+            
+        }
+
         return $ok;
     }
 
